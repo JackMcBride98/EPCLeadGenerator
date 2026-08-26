bad97c77bbef8e5c5cde6d8022a98c4c8129a934
Jack McBride <mcbride.jack1@gmail.com>
2026-08-26 12:00:11 +0100

    Fix the frontend tsc not catching errors in the app had to point it at the tsconfig.app.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Home } from "./features/Home/Home.tsx";
-import { Profile } from "./features/Profile/Profile.tsx";
 
 function App() {
   const queryClient = new QueryClient();
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<h1>Not Found</h1>} />
-          <Route path="/user" element={<Profile />} />
         </Routes>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
