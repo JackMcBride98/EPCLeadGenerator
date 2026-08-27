8ec93ef7bf61fee93f33ef5a3a487152f07e60d9
Jack McBride <37148941+JackMcBride98@users.noreply.github.com>
2026-08-27 12:54:02 +0100

    Search Processed postcodes endpoint and tweak process postcodes (#5)
    * Add endpoint to search processed postcodes, tweak EPCAssessment.cs data model add UPRN, IsLatest and CertificateNumber, update ProcessPostcode to filter out certificates without UPRN and flag latest ones per URPN
    
    * Commit ClientGen
    
    * Format csproj

--- Builders/EPCAssessmentBuilder.cs
+++ Builders/EPCAssessmentBuilder.cs
@@ -13,10 +13,37 @@ public class EPCAssessmentBuilder : Builder<EPCAssessment>
     public string PostcodeKey { get; set; } = Faker.Address.ZipCode().ToUpper();
     public string AddressLine { get; set; } = Faker.Address.StreetAddress();
     public string? EPCRating { get; set; } = Faker.PickRandom("A", "B", "C", "D", "E", "F", "G");
+    public long UniquePropertyReferenceNumber { get; set; } =
+        Faker.Random.Long(100000000000, 999999999999);
+    public string CertificateNumber { get; set; } =
+        Faker.Random.Replace("****-****-****-****-****");
+    public bool IsLatest { get; set; } = true;
     public Instant RegistrationDate { get; set; } =
         SystemClock.Instance.GetCurrentInstant().Minus(Duration.FromDays(365 * 5));
     public Instant UpdatedAt { get; set; } = SystemClock.Instance.GetCurrentInstant();
     public Postcode? Postcode { get; set; } = null;
+
+    public EPCAssessmentBuilder WithIsLatest(bool isLatest)
+    {
+        IsLatest = isLatest;
+        return this;
+    }
+
+    public EPCAssessmentBuilder WithExpired()
+    {
+        RegistrationDate = SystemClock
+            .Instance.GetCurrentInstant()
+            .Minus(Duration.FromDays(365 * 11));
+        return this;
+    }
+
+    public EPCAssessmentBuilder WithExpiringInNextYear()
+    {
+        RegistrationDate = SystemClock
+            .Instance.GetCurrentInstant()
+            .Minus(Duration.FromDays((int)(365 * 9.5)));
+        return this;
+    }
 
     public override EPCAssessment Build()
     {
@@ -26,6 +53,9 @@ public class EPCAssessmentBuilder : Builder<EPCAssessment>
             PostcodeKey = PostcodeKey,
             AddressLine = AddressLine,
             EPCRating = EPCRating,
+            UniquePropertyReferenceNumber = UniquePropertyReferenceNumber,
+            CertificateNumber = CertificateNumber,
+            IsLatest = IsLatest,
             RegistrationDate = RegistrationDate,
             UpdatedAt = UpdatedAt,
             Postcode =
--- EPCLeadGenerator.Api/Database/EPCAssessment.cs
+++ EPCLeadGenerator.Api/Database/EPCAssessment.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq.Expressions;
 using NodaTime;
 
 namespace EPCLeadGenerator.Api.Database;
@@ -7,9 +8,13 @@ namespace EPCLeadGenerator.Api.Database;
 public class EPCAssessment
 {
     public const int EPCAssessmentExpiryDayCount = 365 * 10; // 10 years
+    public const int EPCAssessmentExpiringSoonDayCount = 365 * 9; // 9 years
 
     [Key]
-    public int EPCAssessmentId { get; set; }
+    public long EPCAssessmentId { get; set; }
+
+    [Column("UniquePropertyReferenceNumber")]
+    public long UniquePropertyReferenceNumber { get; set; }
 
     [Column("Postcode")]
     [MaxLength(10)]
@@ -23,14 +28,29 @@ public class EPCAssessment
 
     public Instant RegistrationDate { get; set; }
 
-    public bool IsExpired =>
-        RegistrationDate
-        < SystemClock
-            .Instance.GetCurrentInstant()
-            .Minus(Duration.FromDays(EPCAssessmentExpiryDayCount));
-
     public Instant UpdatedAt { get; set; }
+
+    public bool IsLatest { get; set; }
+
+    [MaxLength(24)]
+    public string CertificateNumber { get; set; }
 
     [ForeignKey(nameof(PostcodeKey))]
     public Postcode Postcode { get; set; } = null!;
+
+    [NotMapped]
+    public bool IsExpired =>
+        IsExpiredExpression.Compile()(this, SystemClock.Instance.GetCurrentInstant());
+
+    [NotMapped]
+    public bool IsExpiringInNextYear =>
+        IsExpiringInNextYearExpression.Compile()(this, SystemClock.Instance.GetCurrentInstant());
+
+    public static Expression<Func<EPCAssessment, Instant, bool>> IsExpiredExpression =>
+        (a, now) => a.RegistrationDate < now.Minus(Duration.FromDays(EPCAssessmentExpiryDayCount));
+
+    public static Expression<Func<EPCAssessment, Instant, bool>> IsExpiringInNextYearExpression =>
+        (a, now) =>
+            a.RegistrationDate >= now.Minus(Duration.FromDays(EPCAssessmentExpiryDayCount))
+            && a.RegistrationDate < now.Minus(Duration.FromDays(EPCAssessmentExpiringSoonDayCount));
 }
